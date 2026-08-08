6f4d798f49f7754ceefd51425b3fe9101d0984b3
Moshe <68689466+Moshe370@users.noreply.github.com>
2026-08-08 19:37:08 -0400

    Variables declared
    next step would be review and then proceeds to calculate the values

---
+++ recreation-one.js
@@ -0,0 +1,27 @@
+console.table([
+  {
+    KATA: "Integers: Recreation One",
+    SRC: "https://www.codewars.com/kata/55aa075506463dac6600010d/train/javascript",
+    DATE: "2026-08-01"
+  }
+])
+
+console.group("## HIPOTESYS n° 1")
+// SET OF LOOPS TO VERIFY AND CALCULATE OF OF THE NUMBERS IN THE SPECIFIED RANGE
+
+m_H1 = 20
+n_H1 = 40
+values_H1 = []
+results_H1 = []
+
+for(var i_H1 = m_H1, x = 0; i <= n_H1, x < n_H1-m_H1; i++, x++) {
+
+  Array.from({length: i_H1}, (_, i) => {
+    if(i_H1 % i == 0) {
+      // arrange the calculus
+    }
+  })
+
+}
+
+console.groupEnd()
