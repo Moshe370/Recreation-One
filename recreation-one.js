23932bfd4e0409fb82c411a77076ea183feb8d12
Moshe <68689466+Moshe370@users.noreply.github.com>
2026-08-12 22:34:47 -0400

    Refactor console output and add hypothesis 2 logic

--- recreation-one.js
+++ recreation-one.js
@@ -17,3 +17,19 @@ array1[0] = array2
 console.log(array1) // yes it can!
 
 console.groupEnd()
+
+console.group("## HIPOTESYS n° 2")
+
+var m_h2 = 20 // M
+var n_h2 = 40 // N
+var pair_h2 = [] // pair of values, if the result of the square root is a integer then the pair is added to the array of results.
+var result_h2 = [] // array of results in which will be saved the pairs.
+
+for(var e = m_h2; e <= n_h2; e++) {
+
+  pair_h2[0] = e
+  // tf this loop will do
+
+} // loop to verify each number between m and n
+
+console.groupEnd()
