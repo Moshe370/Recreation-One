42b0cf91eb67bd905b8f901ebe615c7f4287fc3c
Moshe <68689466+Moshe370@users.noreply.github.com>
2026-08-14 23:20:35 -0400

    Refactor recreation logic and improve logging
    Updated the range for m_h2 and n_h2 and added condition to check for divisors. Adjusted console logs for clarity.

--- recreation-one.js
+++ recreation-one.js
@@ -20,8 +20,8 @@ console.groupEnd()
 
 console.group("## HIPOTESYS n° 2")
 
-var m_h2 = 20 // M
-var n_h2 = 40 // N
+var m_h2 = 1 // M
+var n_h2 = 10 // N
 var values_h2 = []
 var pair_h2 = [] // pair of values, if the result of the square root is a integer then the pair is added to the array of results.
 var result_h2 = [] // array of results in which will be saved the pairs.
@@ -29,14 +29,21 @@ var result_h2 = [] // array of results in which will be saved the pairs.
 for(var e_h2 = m_h2; e_h2 <= n_h2; e_h2++) {
 
   for(var l_h2 = 1; l_h2 <= e_h2; l_h2++) {
-    values_h2.push(l_h2)
+    if(e_h2 % l_h2 == 0) {
+      console.log(l_h2, l_h2**2)
+      values_h2.push(l_h2**2)
+    }
   }
   
+  console.log(values_h2) // there are reapeating values that are making the the sum result in a wrong value for the problem
+
   pair_h2[0] = e_h2
   pair_h2[1] = values_h2.reduce((acc_h2, cur_h2) => acc_h2 + cur_h2, 0)
 
+  console.log(pair_h2)
+
 } // loop to verify each number between m and n
 
-console.log(pair_h2) // the console should be inside the loop for better monitoring but its working, needs to set a condition to consider only the values squared.
+
 
 console.groupEnd()
